985d6cfa0c1ec48d49ca9139b84ff5172d3134d9
Ben Trebilcock <trebi018@umn.edu>
2023-12-05 11:02:14 -0600

    Added images and choices.

--- src/quiz.js
+++ src/quiz.js
@@ -4,43 +4,287 @@ export const sharkQuiz =  {
       question:
       "What type of shark is this?",
       choices:  [
-        "Shark 1",
-        "Shark 2",
-        "Shark 3",
-        "Shark 4",
+        "Tiger shark",
+        "Bull shark",
+        "Great white shark",
+        "Blacktip Shark",
       ],
       type: "MCQs",
-      correctAnswer: "Shark 3",
-      id: 1,
-      cover: "https://upload.wikimedia.org/wikipedia/commons/thumb/5/56/White_shark.jpg/800px-White_shark.jpg?20191104070938",
+      correctAnswer: "Great white shark",
+      cover: "https://t0.gstatic.com/licensed-image?q=tbn:ANd9GcQYsIUlk5w9gylVaPb2kR4mGdVwGd5V7UPCox-J-GTLeg2QVvZwwAHO3zD_IYMJcvz0",
     },
     {
       question:
       "What type of shark is this?",
       choices:  [
-        "Shark 1",
-        "Shark 2",
-        "Shark 3",
-        "Shark 4",
+        "Lemon shark",
+        "Tiger shark",
+        "Nurse shark",
+        "Bull shark",
       ],
       type: "MCQs",
-      correctAnswer: "Shark 3",
-      id: 1,
-      cover: "https://media.istockphoto.com/id/1130525042/photo/white-shark-marine-predator.jpg?s=612x612&w=0&k=20&c=JOoHq51ETiHP4kJzXJ8Curc69jGPxM6hyZNpYTFzsgk=",
+      correctAnswer: "Tiger shark",
+      cover: "https://upload.wikimedia.org/wikipedia/commons/thumb/3/39/Tiger_shark.jpg/640px-Tiger_shark.jpg",
     },
     {
       question:
       "What type of shark is this?",
       choices:  [
-        "Shark 1",
-        "Shark 2",
-        "Shark 3",
-        "Shark 4",
+        "Basking shark",
+        "Great white shark",
+        "Hammerhead shark",
+        "Whale shark",
       ],
       type: "MCQs",
-      correctAnswer: "Shark 3",
-      id: 1,
-      cover: "https://media.wired.com/photos/5c6750d23e8add2cdb91724f/master/pass/shark-551025353.jpg",
+      correctAnswer: "Whale shark",
+      cover: "https://t0.gstatic.com/licensed-image?q=tbn:ANd9GcTCw9BMTPvnnTWWgBV1lNvEmPFEQPogjVTFCrrnQHDcF0_8zMQJnjwZ1T0ob1gJmt98",
+    },
+    {
+      question:
+      "What type of shark is this?",
+      choices:  [
+        "Bull shark",
+        "Blacktip shark",
+        "Tiger shark",
+        "Nurse shark",
+      ],
+      type: "MCQs",
+      correctAnswer: "Bull shark",
+      cover: "https://upload.wikimedia.org/wikipedia/commons/thumb/8/83/Carcharhinus_leucas%2C_Koh_Phangan.jpg/800px-Carcharhinus_leucas%2C_Koh_Phangan.jpg",
+    },
+    {
+      question:
+      "What type of shark is this?",
+      choices:  [
+        "Dusky shark",
+        "Nurse shark",
+        "Blacktip reef shark",
+        "Blue shark",
+      ],
+      type: "MCQs",
+      correctAnswer: "Blacktip reef shark",
+      cover: "https://upload.wikimedia.org/wikipedia/commons/0/01/Carcharhinus_melanopterus_mirihi.jpg",
+    },
+    {
+      question:
+      "What type of shark is this?",
+      choices:  [
+        "Frilled shark",
+        "Basking shark",
+        "Hammerhead shark",
+        "Whale shark",
+      ],
+      type: "MCQs",
+      correctAnswer: "Basking shark",
+      cover: "https://upload.wikimedia.org/wikipedia/commons/0/0b/Cetorhinus_maximus_by_greg_skomal.JPG",
+    },
+    {
+      question:
+      "What type of shark is this?",
+      choices:  [
+        "Zebra shark",
+        "Grey reef shark",
+        "Blacktip shark",
+        "Blue shark",
+      ],
+      type: "MCQs",
+      correctAnswer: "Blacktip shark",
+      cover: "https://t1.gstatic.com/licensed-image?q=tbn:ANd9GcT0Lre2LSzwKtSOFlQ-uJS1bVkctfFung6_lx0EF_ZqxE1mpdfuiZ6H9mhEfb3wmcgA",
+    },
+    {
+      question:
+      "What type of shark is this?",
+      choices:  [
+        "Blue shark",
+        "Bull shark",
+        "Sandbar shark",
+        "Greenland shark",
+      ],
+      type: "MCQs",
+      correctAnswer: "Blue shark",
+      cover: "https://upload.wikimedia.org/wikipedia/commons/b/b8/Tibur%C3%B3n_azul_%28Prionace_glauca%29%2C_canal_Fayal-Pico%2C_islas_Azores%2C_Portugal%2C_2020-07-27%2C_DD_28.jpg",
+    },
+    {
+      question:
+      "What type of shark is this?",
+      choices:  [
+        "Shortfin mako shark",
+        "Blacktip reef shark",
+        "Grey reef shark",
+        "Whitetip reef shark",
+      ],
+      type: "MCQs",
+      correctAnswer: "Whitetip reef shark",
+      cover: "https://upload.wikimedia.org/wikipedia/commons/9/95/Carcharhinus_albimarginatus-shark.jpg",
+    },
+    {
+      question:
+      "What type of shark is this?",
+      choices:  [
+        "Tiger shark",
+        "Silky shark",
+        "Blue shark",
+        "Sandbar shark",
+      ],
+      type: "MCQs",
+      correctAnswer: "Silky shark",
+      cover: "https://upload.wikimedia.org/wikipedia/commons/9/9f/Carcharhinus_falciformis_off_Cuba.jpg",
+    },
+    {
+      question:
+      "What type of shark is this?",
+      choices:  [
+        "Frilled shark",
+        "Sand tiger shark",
+        "Lemon shark",
+        "Greenland shark",
+      ],
+      type: "MCQs",
+      correctAnswer: "Sand tiger shark",
+      cover: "https://upload.wikimedia.org/wikipedia/commons/9/9a/Carcharias_taurus_SI.jpg",
+    },
+    {
+      question:
+      "What type of shark is this?",
+      choices:  [
+        "Great white shark",
+        "Nurse shark",
+        "Grey reef shark",
+        "Dusky shark",
+      ],
+      type: "MCQs",
+      correctAnswer: "Grey reef shark",
+      cover: "https://upload.wikimedia.org/wikipedia/commons/a/a0/Grey5b.jpg",
+    },
+    {
+      question:
+      "What type of shark is this?",
+      choices:  [
+        "Lemon shark",
+        "Dusky shark",
+        "Sandbar shark",
+        "Zebra shark",
+      ],
+      type: "MCQs",
+      correctAnswer: "Zebra shark",
+      cover: "https://www.virginiaaquarium.com/assets/DSC00340-20220927.jpg",
+    },
+    {
+      question:
+      "What type of shark is this?",
+      choices:  [
+        "Sandbar shark",
+        "Dusky shark",
+        "Greenland shark",
+        "Nurse shark",
+      ],
+      type: "MCQs",
+      correctAnswer: "Sandbar shark",
+      cover: "https://upload.wikimedia.org/wikipedia/commons/6/6e/Carcharhinus_plumbeus_georgia.jpg",
+    },
+    {
+      question:
+      "What type of shark is this?",
+      choices:  [
+        "Basking shark",
+        "Shortfin mako shark",
+        "Tiger shark",
+        "Blue shark",
+      ],
+      type: "MCQs",
+      correctAnswer: "Shortfin mako shark",
+      cover: "https://upload.wikimedia.org/wikipedia/commons/2/2f/Isurus_oxyrinchus_by_mark_conlin2.JPG",
+    },
+    {
+      question:
+      "What type of shark is this?",
+      choices:  [
+        "Nurse shark",
+        "Zebra shark",
+        "Grey reef shark",
+        "Lemon shark",
+      ],
+      type: "MCQs",
+      correctAnswer: "Nurse shark",
+      cover: "https://upload.wikimedia.org/wikipedia/commons/d/d1/Nurse_shark.jpg",
+    },
+    {
+      question:
+      "What type of shark is this?",
+      choices:  [
+        "Basking shark",
+        "Dusky shark",
+        "Greenland shark",
+        "Silky shark",
+      ],
+      type: "MCQs",
+      correctAnswer: "Greenland shark",
+      cover: "https://cdn.britannica.com/11/181611-050-0494BE9F/Greenland-shark-ice-Lancaster-Sound-Baffin-Island.jpg",
+    },
+    {
+      question:
+      "What type of shark is this?",
+      choices:  [
+        "Bull shark",
+        "Tiger shark",
+        "Hammerhead shark",
+        "Whale shark",
+      ],
+      type: "MCQs",
+      correctAnswer: "Hammerhead shark",
+      cover: "https://i.natgeofe.com/n/aa12774c-6281-4f96-988e-b9be88deaf88/hammerhead-sharks_square.jpg",
+    },
+    {
+      question:
+      "What type of shark is this?",
+      choices:  [
+        "Lemon shark",
+        "Sandbar shark",
+        "Frilled shark",
+        "Nurse shark",
+      ],
+      type: "MCQs",
+      correctAnswer: "Lemon shark",
+      cover: "https://upload.wikimedia.org/wikipedia/en/thumb/5/55/Lemonshark.JPG/1200px-Lemonshark.JPG",
+    },
+    {
+      question:
+      "What type of shark is this?",
+      choices:  [
+        "Zebra shark",
+        "Frilled shark",
+        "Shortfin mako shark",
+        "Silky shark",
+      ],
+      type: "MCQs",
+      correctAnswer: "Frilled shark",
+      cover: "https://t2.gstatic.com/licensed-image?q=tbn:ANd9GcRXSHFXm5HYR_LAfcB0v_BztcgSisz2QU9s8fCcf5jGgHDRkMxBFqws-BDElQljzeVY",
+    },
+    {
+      question:
+      "What type of shark is this?",
+      choices:  [
+        "Blacktip shark",
+        "Bull shark",
+        "Hammerhead shark",
+        "Great hammerhead shark",
+      ],
+      type: "MCQs",
+      correctAnswer: "Great hammerhead shark",
+      cover: "https://upload.wikimedia.org/wikipedia/commons/b/bd/Great_hammerhead2.jpg",
+    },
+    {
+      question:
+      "What type of shark is this?",
+      choices:  [
+        "Bull shark",
+        "Dusky shark",
+        "Great white shark",
+        "Blue shark",
+      ],
+      type: "MCQs",
+      correctAnswer: "Dusky shark",
+      cover: "https://upload.wikimedia.org/wikipedia/commons/d/d3/Carcharhinus_obscurus_at_Seaworld.jpg",
     },
   ]
 };
